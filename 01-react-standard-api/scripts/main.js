39cde74aeb5a8bc3aef10539799ab3593e75581a
dongwonAhn <ado2188@naver.com>
2025-08-11 15:52:27 +0900

    learn. React Standard API - React.createElement(type, props, ...children) - ReactDOM.createRoot(container)

--- 01-react-standard-api/scripts/main.js
+++ 01-react-standard-api/scripts/main.js
@@ -1,33 +1,33 @@
-import { createElement as h } from './lib/Euid.js'
-import { createRoot } from './lib/EuidDOM.js'
+const { createElement: h } = React;
+const { createRoot } = ReactDOM;
 
-const h1Element = h('h1', {}, h('code', {}, 'bunx'), ' 명령')
+const h1Element = h("h1", {}, h("code", {}, "bunx"), " 명령");
 
 const pElement = h(
-  'p',
+  "p",
   {},
   h(
-    'a',
+    "a",
     {
-      href: 'https:bun.sh',
-      rel: 'noopener noreferrer',
-      target: '_blank',
+      href: "https:bun.sh",
+      rel: "noopener noreferrer",
+      target: "_blank",
     },
-    'Bun'
+    "Bun"
   ),
-  '의 빠른 시작 덕분에, ',
-  h('code', {}, 'bunx'),
-  '는 로컬에 설치된 패키지 실행 시 ',
-  h('code', {}, 'npx'),
-  '보다 약 100배 빠릅니다.'
-)
+  "의 빠른 시작 덕분에, ",
+  h("code", {}, "bunx"),
+  "는 로컬에 설치된 패키지 실행 시 ",
+  h("code", {}, "npx"),
+  "보다 약 100배 빠릅니다."
+);
 
 const app = h(
-  'section',
-  { className: 'bunx-introduction' },
+  "section",
+  { className: "bunx-introduction" },
   h1Element,
   pElement
-)
+);
 
-const root = createRoot(document.querySelector('main'))
-root.render(app)
+const root = createRoot(document.querySelector("main"));
+root.render(app);
