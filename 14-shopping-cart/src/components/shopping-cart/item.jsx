36100635a4b61754f35bd85b5cf60ce26d44e426
dongwonAhn <ado2188@naver.com>
2025-08-20 16:13:10 +0900

    learn. 상태 관리, 상태 끌어올리기 - 상태 없는 컴포넌트 - 상태 있는 컴포넌트 - 상위 컴포넌트에서 상태를 관리하고 하위 컴포넌트에 전달

--- 14-shopping-cart/src/components/shopping-cart/item.jsx
+++ 14-shopping-cart/src/components/shopping-cart/item.jsx
@@ -1,10 +1,28 @@
 import CartCounter from './counter'
 
-export default function CartItem() {
+export default function CartItem({ product, setProducts }) {
+  const handleUpdateQuantity = (amount) => {
+    setProducts((draft) => {
+      const updateProduct = draft.find((p) => p.id === product.id)
+      updateProduct.quantity += amount
+    })
+  }
+
   return (
     <li className="flex gap-2 justify-between py-2 px-0.5 text-[18px]">
-      상품 항목
-      <CartCounter />
+      <p className="flex gap-1.5 items-center">
+        <img
+          className="size-8 rounded-full"
+          src={`/products/${product.imageUrl}`}
+          alt=""
+        />
+        <strong>{product.name}</strong> {product.price.toLocaleString()}원
+      </p>
+      <CartCounter
+        quantity={product.quantity}
+        updateQuantity={handleUpdateQuantity}
+        inventory={product.inventory}
+      />
     </li>
   )
 }
--- 14-shopping-cart/src/components/shopping-cart/list.jsx
+++ 14-shopping-cart/src/components/shopping-cart/list.jsx
@@ -1,11 +1,21 @@
+import { useImmer } from 'use-immer'
+import INITIAL_PRODUCTS from '@/data/cart.json'
 import CartItem from './item'
 
 export default function CartList() {
+  const [products, setProducts] = useImmer(INITIAL_PRODUCTS)
+
+  console.log(products.at(0).quantity)
+
   return (
     <ul>
-      <CartItem />
-      <CartItem />
-      <CartItem />
+      {products.map((product) => (
+        <CartItem
+          key={product.id}
+          product={product}
+          setProducts={setProducts}
+        />
+      ))}
     </ul>
   )
 }
